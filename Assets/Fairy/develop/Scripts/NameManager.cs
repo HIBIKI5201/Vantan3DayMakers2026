8fa1deab87920afe6ee333407e544cedc63c4188
tomo-1230 <satokawa1130tomoki2@gmail.com>
2026-03-05 12:24:41 +0900

    タイトル修正

---
+++ Assets/Fairy/develop/Scripts/NameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class NameManager : MonoBehaviour
+{
+    private DataManager _dataManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _dataManager = FindAnyObjectByType<DataManager>();
+    }
+     public void SetName(string name)
+    {
+        _dataManager.SetName(name);
+    }
+}
